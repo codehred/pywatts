010dbe32ca5a7edee2d2333e8b0d7c52b6dd2bd8
Karol Martínez <kmtz20056@gmail.com>
2025-12-09 10:15:06 -0600

    Lógica actualizada en app y conraseña hasheada para futura implementación de DB

--- sim_pywatts/app.py
+++ sim_pywatts/app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, redirect, request, session
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = 'dev'
+
+
+VALID_USERNAME = 'user'
+VALID_PASSWORD_HASH = hashlib.sha256('pass'.encode()).hexdigest()
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        if username:
-            session['username'] = username
+        password = request.form['password']
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        
+        if username == VALID_USERNAME and password_hash == VALID_PASSWORD_HASH:
+            session['user'] = username
             return redirect('/dashboard')
     return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
-    if 'username' not in session:
+    if 'user' not in session:
         return redirect('/')
     if request.method == 'POST':
         device = request.form['device']
@@ -23,7 +31,7 @@ def dashboard():
         energy_consumed = float(power) * float(hours)
         print(device, energy_consumed)
 
-    return render_template('dashboard.html', username=session['username'])
+    return render_template('dashboard.html', username=session['user'])
 
 if __name__ == '__main__':
     app.run(debug=True)
